eda00b7bc17ffdbe27c1b6a422fa7bb1d864fe77
Ahaan Dabholkar <ahaandabholkar@gmail.com>
2023-02-17 18:46:29 -0500

    remove a few TODOs that have already been finished

--- src/property_container.cc
+++ src/property_container.cc
@@ -5,14 +5,8 @@
 // theses `Values`.
 // This data model is very primitive as of now and the ownership model is expected to
 // change as well.
-// TODO think about using smart pointers for `Value` ownership.
-// Hence for now, `Values` can dereference to null which is unsafe.
-
 
 // having null_value_ on the stack as it is static makes sense ?
-// FIXME the constructor for `PropertyContainer` will deallocate all initialised elts
-// to prevent memory leaks. Throw exception to signal an error to caller?
-
 
 NullValue PropertyContainer::null_value_ = NullValue();
 
@@ -25,6 +19,8 @@ NullValue PropertyContainer::null_value_ = NullValue();
 //   }
 // }
 
+
+// insert a property into the container and return true if it succeeds or false otherwise
 bool PropertyContainer::add_property(std::string property_name, std::unique_ptr<Value> value)
 {
   return properties_.insert({property_name, std::move(value)}).second ;
@@ -34,7 +30,7 @@ bool PropertyContainer::add_property(std::string property_name, std::unique_ptr<
 // the destructor clears the memory assoc with the `Value` subtype. The assumption is that
 // the container has been initialised with objects that have been dynamically allocated.
 // TODO have the initialiser of `Value` always initialise it on heap 
-PropertyContainer::~PropertyContainer()
+PropertyContainer::~PropertyContainer() noexcept
 {
   // for(auto &elt: properties_) elt.second->clear();
   properties_.clear();
