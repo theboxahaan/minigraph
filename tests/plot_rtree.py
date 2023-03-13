804e11868d79c86a37c128937468ec593e01586d
Ahaan Dabholkar <ahaandabholkar@gmail.com>
2023-03-13 13:27:24 -0400

    added viz. for `rec.log`; fmt:`x, x_len, y, y_len`

---
+++ tests/plot_rtree.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+
+
+if __name__ == '__main__':
+  with open('rec.log', 'r') as fd:
+    s = []
+    for l in fd.readlines():
+      s.append(l.split(',')[:-1])
+  fig, ax = plt.subplots()
+  ax.plot([0,700], [0,700])
+  for x, x_len, y, y_len in s:
+    ax.add_patch(Rectangle((int(x), int(y)), int(x_len), int(y_len), facecolor='none', edgecolor='red'))
+  plt.show()
+ 
+
